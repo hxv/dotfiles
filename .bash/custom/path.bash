7a5cda3dddcebbeae4a769152de7ffa8bf405fb0
Michał Włodarczyk <admin@hxv.me>
2019-04-03 09:23:20 +0200

    Added alternative composer path to PATH.

--- .bash/custom/path.bash
+++ .bash/custom/path.bash
@@ -1,6 +1,7 @@
 #/usr/bin/env bash
 
 export PATH="$PATH:~/.config/composer/vendor/bin"
+export PATH="$PATH:~/.composer/vendor/bin"
 export PATH="$PATH:~/.go/bin/"
 export PATH="$PATH:~/.local/bin/"
 export PATH="$PATH:~/bin/"
