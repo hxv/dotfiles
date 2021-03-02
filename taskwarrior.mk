2fd942515b0f4fa82db0e4f0d62c8719cebbdd26
Michał Włodarczyk <admin@hxv.me>
2021-03-02 19:32:47 +0100

    Add timewarrior to taskwarrior + custom script to track time.

--- taskwarrior.mk
+++ taskwarrior.mk
@@ -1,12 +1,19 @@
 all_targets += taskwarrior
 
-taskwarrior_files = ~/.taskrc ~/.taskwarrior/holidays.rc
+taskwarrior_files = ~/.taskrc ~/.taskwarrior/holidays.rc ~/bin/task-rofi
 
-taskwarrior: /usr/bin/task $(taskwarrior_files)
+taskwarrior: /usr/bin/task /usr/bin/timew $(taskwarrior_files) rofi ~/.task/hooks/on-modify.timewarrior
 .PHONY: taskwarrior
 
 /usr/bin/task:
 	$(call install,taskwarrior)
+
+/usr/bin/timew:
+	$(call install,timewarrior)
+
+~/.task/hooks/on-modify.timewarrior:
+	cp /usr/share/doc/timewarrior/ext/on-modify.timewarrior $@
+	chmod +x $@
 
 $(taskwarrior_files):
 	$(call homelink,$@)
