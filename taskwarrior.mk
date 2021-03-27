87ef4e773f16d656e139e3db46b2fd50b28b5a04
Michał Włodarczyk <admin@hxv.me>
2021-03-27 11:28:21 +0100

    Installing taskwarrior and timewarrior from sources.

--- taskwarrior.mk
+++ taskwarrior.mk
@@ -6,10 +6,26 @@ taskwarrior: /usr/bin/task /usr/bin/timew $(taskwarrior_files) rofi
 .PHONY: taskwarrior
 
 /usr/bin/task:
-	$(call install,taskwarrior)
+	cd /tmp && \
+	  rm -rf taskwarrior && \
+	  git clone https://github.com/GothenburgBitFactory/taskwarrior.git --branch=master && \
+	  cd taskwarrior && \
+	  $(call install,libgnutls28-dev) && \
+	  $(call install,uuid-dev) && \
+	  cmake -DCMAKE_BUILD_TYPE=release . && \
+	  make && \
+	  sudo make install && \
+	  rm -rf /tmp/taskwarrior
 
 /usr/bin/timew:
-	$(call install,timewarrior)
+	cd /tmp && \
+	  rm -rf timewarrior && \
+	  git clone --recurse-submodules https://github.com/GothenburgBitFactory/timewarrior.git --branch=master && \
+	  cd timewarrior && \
+	  cmake -DCMAKE_BUILD_TYPE=release . && \
+	  make && \
+	  sudo make install && \
+	  rm -rf /tmp/timewarrior
 
 $(taskwarrior_files):
 	$(call homelink,$@)
