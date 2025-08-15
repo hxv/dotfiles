efa043b4d55f98fb0c23af576eaa571b1a29bade
Michał Włodarczyk <admin@hxv.me>
2025-08-15 17:58:40 +0200

    Bump PHP version to 8.4.

--- php.mk
+++ php.mk
@@ -2,22 +2,22 @@ all_targets += php
 
 php_ppa = /etc/apt/sources.list.d/ondrej-ubuntu-php-$(ubuntu_codename).list
 
-php: /usr/bin/php8.1 ~/bin/composer ~/.config/composer/vendor/bin/psysh
+php: /usr/bin/php8.4 ~/bin/composer ~/.config/composer/vendor/bin/psysh
 .PHONY: php
 
 $(php_ppa):
 	sudo add-apt-repository -y ppa:ondrej/php
 
-/usr/bin/php8.1: $(php_ppa)
-	sudo apt install -y php8.1-cli php8.1-amqp php8.1-ast php8.1-bcmath php8.1-bz2 php8.1-curl \
-		php8.1-dba php8.1-dev php8.1-gd php8.1-gmp php8.1-imagick php8.1-imap php8.1-intl \
-		php8.1-ldap php8.1-mbstring php8.1-memcache php8.1-memcached php8.1-mongodb \
-		php8.1-mysql php8.1-odbc php8.1-opcache php8.1-pgsql php8.1-readline php8.1-redis \
-		php8.1-soap php8.1-sqlite3 php8.1-uuid php8.1-xdebug php8.1-xml php8.1-xsl \
-		php8.1-yaml php8.1-zip
+/usr/bin/php8.4: #$(php_ppa)
+	sudo apt install -y php8.4-cli php8.4-amqp php8.4-ast php8.4-bcmath php8.4-bz2 php8.4-curl \
+		php8.4-dba php8.4-dev php8.4-gd php8.4-gmp php8.4-imagick php8.4-imap php8.4-intl \
+		php8.4-ldap php8.4-mbstring php8.4-memcache php8.4-memcached php8.4-mongodb \
+		php8.4-mysql php8.4-odbc php8.4-opcache php8.4-pgsql php8.4-readline php8.4-redis \
+		php8.4-soap php8.4-sqlite3 php8.4-uuid php8.4-xdebug php8.4-xml php8.4-xsl \
+		php8.4-yaml php8.4-zip
 	sudo touch $@
 
-~/bin/composer: /usr/bin/php8.1
+~/bin/composer: /usr/bin/php8.4
 	wget -O /tmp/composer-setup https://getcomposer.org/installer
 	rm -rf ~/bin/composer
 	mkdir -p ~/bin
